0e31f1bfb7df04568acbb804b57cc967392660f2
tranhoanghuy123 <106876476+tranhoanghuy123@users.noreply.github.com>
2022-10-13 09:04:30 +0700

    Add files via upload

---
+++ Client/Client.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.client_result = new System.Windows.Forms.TextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.client_Message = new System.Windows.Forms.TextBox();
+            this.client_Port = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.client_IP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.btn_Disconnect = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // client_result
+            // 
+            this.client_result.Enabled = false;
+            this.client_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client_result.Location = new System.Drawing.Point(32, 316);
+            this.client_result.Multiline = true;
+            this.client_result.Name = "client_result";
+            this.client_result.Size = new System.Drawing.Size(637, 196);
+            this.client_result.TabIndex = 12;
+            this.client_result.TextChanged += new System.EventHandler(this.client_result_TextChanged);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Enabled = false;
+            this.btn_Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Send.Location = new System.Drawing.Point(139, 266);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(213, 30);
+            this.btn_Send.TabIndex = 10;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Connect.Location = new System.Drawing.Point(139, 161);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(98, 30);
+            this.btn_Connect.TabIndex = 11;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // client_Message
+            // 
+            this.client_Message.Enabled = false;
+            this.client_Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client_Message.Location = new System.Drawing.Point(139, 218);
+            this.client_Message.Name = "client_Message";
+            this.client_Message.Size = new System.Drawing.Size(530, 26);
+            this.client_Message.TabIndex = 7;
+            // 
+            // client_Port
+            // 
+            this.client_Port.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client_Port.Location = new System.Drawing.Point(139, 68);
+            this.client_Port.Name = "client_Port";
+            this.client_Port.Size = new System.Drawing.Size(213, 26);
+            this.client_Port.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message: ";
+            // 
+            // client_IP
+            // 
+            this.client_IP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.client_IP.Location = new System.Drawing.Point(139, 27);
+            this.client_IP.Name = "client_IP";
+            this.client_IP.Size = new System.Drawing.Size(213, 26);
+            this.client_IP.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Port Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "IP Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Username:";
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.Location = new System.Drawing.Point(139, 116);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(213, 26);
+            this.txt_Username.TabIndex = 8;
+            // 
+            // btn_Disconnect
+            // 
+            this.btn_Disconnect.Enabled = false;
+            this.btn_Disconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Disconnect.Location = new System.Drawing.Point(252, 161);
+            this.btn_Disconnect.Name = "btn_Disconnect";
+            this.btn_Disconnect.Size = new System.Drawing.Size(100, 30);
+            this.btn_Disconnect.TabIndex = 13;
+            this.btn_Disconnect.Text = "Disconnect";
+            this.btn_Disconnect.UseVisualStyleBackColor = true;
+            this.btn_Disconnect.Click += new System.EventHandler(this.btn_Disconnect_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Enabled = false;
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(456, 266);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(213, 29);
+            this.btn_Clear.TabIndex = 14;
+            this.btn_Clear.Text = "Clear Screen";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Gold;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(438, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 25);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Author: An Đập Choai";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(438, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 25);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Author: An IT";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Gold;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(438, 28);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(195, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Author: An khoai to";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 524);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Disconnect);
+            this.Controls.Add(this.client_result);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.client_Message);
+            this.Controls.Add(this.txt_Username);
+            this.Controls.Add(this.client_Port);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.client_IP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox client_result;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.TextBox client_Message;
+        private System.Windows.Forms.TextBox client_Port;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox client_IP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Button btn_Disconnect;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Client/Client.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        TcpClient client;
+        byte[] message;
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            IPAddress ip;
+            int port;
+            if (string.IsNullOrEmpty(client_IP.Text) || string.IsNullOrEmpty(client_Port.Text))
+            {
+                MessageBox.Show("Vui Lòng Nhập IP Và Port Từ 1000 Trở Lên!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!int.TryParse(client_Port.Text, out port))
+            {
+                MessageBox.Show("Port Không Hợp Lệ!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //if (!IPAddress.TryParse(client_IP.Text, out ip))
+            //{
+            //    MessageBox.Show("Vui Lòng Nhập IP Hợp Lệ!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    return;
+            //}
+            if (string.IsNullOrEmpty(txt_Username.Text))
+            {
+                MessageBox.Show("Tên Người Dùng Bị Bỏ Trống Kìa :((", "Cảnh Báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            client_result.Text += "Waiting for Server... \r\n";
+            client = new TcpClient();
+            IPAddress[] host = Dns.GetHostAddresses(client_IP.Text);
+            var result = client.BeginConnect(host[0], port, Connect, client); 
+            client_result.Text += "Connected "+ client_IP.Text + " !\r\n";
+
+            // Config form non-input data,only using textbox message and button send then start server
+            client_IP.Enabled = false;
+            client_Port.Enabled = false;
+            btn_Connect.Enabled = false;
+            txt_Username.Enabled = false;
+            client_Message.Enabled = true;
+            btn_Send.Enabled = true; 
+            btn_Clear.Enabled = true;
+            btn_Disconnect.Enabled = true;
+        }
+        void Connect(IAsyncResult iar)
+        {
+            TcpClient TCPClient;
+
+            try
+            {
+                TCPClient = (TcpClient)iar.AsyncState;
+                TCPClient.EndConnect(iar);
+                message = new byte[512];
+                // Read Message if connection is successful
+                TCPClient.GetStream().BeginRead(message, 0, message.Length, readMessageFromServer, TCPClient); 
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void readMessageFromServer(IAsyncResult iar)
+        {
+            TcpClient TCPClient;
+            int nCountBytesReceivedFromServer;
+            string strReceived;
+
+            try
+            {
+                TCPClient = (TcpClient)iar.AsyncState;
+                nCountBytesReceivedFromServer = TCPClient.GetStream().EndRead(iar); // Get quantity bytes of Network stream
+
+                if (nCountBytesReceivedFromServer == 0) 
+                {
+
+                    MessageBox.Show("Kết Nối Thất Bại.", "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //Get Message From server and Display it 
+                strReceived = Encoding.UTF8.GetString(message, 0, nCountBytesReceivedFromServer);
+                msg(strReceived);
+                message = new byte[512];
+                TCPClient.GetStream().BeginRead(message, 0, message.Length, readMessageFromServer, TCPClient);
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void writeMessageToServer(IAsyncResult iar)
+        {
+            TcpClient TCPClient;
+            try
+            {
+                TCPClient = (TcpClient)iar.AsyncState;
+                TCPClient.GetStream().EndWrite(iar);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            //Events will do something if Button is clicked
+            if (string.IsNullOrEmpty(client_Message.Text)) // check message is empty?
+            {
+                MessageBox.Show("Vui lòng nhập nội dung tin nhắn!", "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            };
+            try
+            {
+                if (client != null)
+                {
+                    if (client.Client.Connected)
+                    {
+                        byte[] message = Encoding.UTF8.GetBytes(txt_Username.Text+" :" + client_Message.Text);
+                        //get stream and send message to server
+                        client.GetStream().BeginWrite(message, 0, message.Length, writeMessageToServer, client);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Thông báo!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        public void msg(string mesg)
+        {
+            // Method display Message into textbox_result
+            client_result.Text += mesg + "\r\n";
+        }
+        public void Disconnect(IAsyncResult iar)
+        {
+            client.EndConnect(iar);
+        }
+        private void btn_Disconnect_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ngắt Kết Nối Thành Công!","Thông Báo",MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            btn_Disconnect.Enabled = false;
+            btn_Clear.Enabled = false;
+            client_Message.Enabled = false;
+            btn_Send.Enabled = false;
+            client_result.Enabled = false;
+            client.GetStream().Close();
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            client_result.Text = null;
+        }
+
+        private void client_result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
